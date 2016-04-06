7ca2d710ce85405172a650a57ae9030e946806d0
Kyle Espinola <ksespinola@gmail.com>
2016-04-05 18:27:05 -0700

    create add_user migration with login

--- db/migrations/20160403092929_add_users.js
+++ db/migrations/20160403092929_add_users.js
@@ -1,13 +1,28 @@
 exports.up = function (knex) {
   return knex.schema
-    .createTable('Users', function (table) {
-      table.increments('id').primary();
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-      table.string('first_name');
-      table.string('last_name');
+    .createTable('users', function (user) {
+      user.increments('id').primary();
+      user.timestamp('created_at').defaultTo(knex.fn.now());
+      user.string('first_name');
+      user.string('last_name');
     })
+    .then(function () {
+      return knex.schema
+        .createTable('logins', function (login) {
+          login.increments('id').primary();
+          login.timestamp('created_at').defaultTo(knex.fn.now());
+          login.timestamp('updated_at').nullable();
+          login.integer('user_id').unsigned().references('users.id').onDelete('CASCADE');
+          login.string('email');
+          login.string('password_hash');
+        });
+    });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable('Users');
+  return knex.schema
+    .dropTable('users')
+    .then(function () {
+      return knex.schema.dropTable('logins');
+    });
 };
--- db/seeds/dev/sample_user.js
+++ db/seeds/dev/sample_user.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   return Promise.join(
-    knex('Users').del(),
-    knex('Users').insert({ first_name: 'John', last_name: 'Doe' })
+    knex('users').del(),
+    knex('users').insert({ first_name: 'John', last_name: 'Doe' })
   );
 };
